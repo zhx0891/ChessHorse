c5aa1b98e0e0511d532c2827659fbdba5daaf4ed
zhukovv <northbug@yandex.ru>
2022-08-29 16:17:12 +0500

    прописаны проверка клетки и присвоение клетке значения

--- Horse.cs
+++ Horse.cs
@@ -1,58 +1,94 @@
 public static class Horse
 {
-    public static (int x, int y) nextMove( int x, int y, int size, int [,] field)
+    // public static (bool empCell, int x, int y ) valueCell (int [,] field, int x, int y)
+    // {
+    //  bool empCell=true;   
+    //  return (empCell , x, y);
+    // }
+    public static (int x, int) nextMove(int x, int y, int size, int[,] field, int count)
     {
+        
         //вверх вправо
         if (((x - 2) >= 0) && ((y + 1) < size))
         {
-            x = x - 2;
-            y = y + 1;
-            if(field[x,y] != 1)
+            if (field[x - 2, y + 1] != 1)
             {
-                field[x,y]=1;
+                x = x - 2;
+                y = y + 1;
+                field[x, y] = 1;
+                count++;
             }
         }
         //вверх влево
-        if (((x - 2) > 0) && ((y - 1) > 0))
+        else if (((x - 2) > 0) && ((y - 1) > 0))
         {
-            x = x - 2;
-            y = y - 1;
+            if (field[x - 2, y - 1] != 1)
+            {
+                x = x - 2;
+                y = y - 1;
+                field[x, y] = 1;
+            }
         }
         //вправо вниз
-        if (((x + 1) < size ) && ((y + 2) < size))
+        if (((x + 1) < size) && ((y + 2) < size))
         {
-            x = x + 1;
-            y = y + 2;
+            if (field[x + 1, y + 2] != 1)
+            {
+                x = x + 1;
+                y = y + 2;
+                field[x, y] = 1;
+            }
         }
         //вправо вверх
-        if (((x - 1) <= 0 ) && ((y + 2) < size))
+        if (((x - 1) <= 0) && ((y + 2) < size))
         {
-            x = x - 1;
-            y = y + 2;
+            if (field[x - 1, y + 2] != 1)
+            {
+                x = x - 1;
+                y = y + 2;
+                field[x, y] = 1;
+            }
         }
         //вниз влево
-        if (((x + 2) < size ) && ((y - 1) >= 0))
+        if (((x + 2) < size) && ((y - 1) >= 0))
         {
-            x = x + 2;
-            y = y - 1;
+            if (field[x + 2, y - 1] != 1)
+            {
+                x = x + 2;
+                y = y - 1;
+                field[x, y] = 1;
+            }
         }
         //вниз вправо
-        if (((x + 2) < size ) && ((y + 1) < size ))
+        if (((x + 2) < size) && ((y + 1) < size))
         {
-            x = x + 2;
-            y = y + 1;
+            if (field[x + 2, y + 1] != 1)
+            {
+                x = x + 2;
+                y = y + 1;
+                field[x, y] = 1;
+            }
         }
         //влево вверх
-        if (((x - 1) >= 0 ) && ((y - 2) >= 0))
+        if (((x - 1) >= 0) && ((y - 2) >= 0))
         {
-            x = x - 1;
-            y = y - 2;
+            if (field[x - 1, y - 2] != 1)
+            {
+                x = x - 1;
+                y = y - 2;
+                field[x, y] = 1;
+            }
         }
         //влево вниз
-        if (((x + 1) < size ) && ((y - 2) >= 0))
+        if (((x + 1) < size) && ((y - 2) >= 0))
         {
-            x = x + 1;
-            y = y - 2;
+            if (field[x + 2, y + 1] != 1)
+            {
+                x = x + 1;
+                y = y - 2;
+                field[x, y] = 1;
+            }
+
         }
 
         return (x, y);
