5a7fa865532f5c6395bcd75b7cedbc7db9bd600e
zhukovv <northbug@yandex.ru>
2022-08-29 15:31:20 +0500

    прописаны условия проверки поля

--- Horse.cs
+++ Horse.cs
@@ -1,36 +1,60 @@
 public static class Horse
 {
-    public static string moveHorse(int[,] field, int x, int y, int size)
+    public static (int x, int y) nextMove( int x, int y, int size, int [,] field)
     {
-        string result = "Begin";
-        int preX=0;
-        int preY=0;
-
         //вверх вправо
         if (((x - 2) >= 0) && ((y + 1) < size))
         {
-            preX=x;
-            preY=y;
             x = x - 2;
             y = y + 1;
+            if(field[x,y] != 1)
+            {
+                field[x,y]=1;
+            }
         }
         //вверх влево
-        else if (((x - 2) > 0) && ((y - 1) > 0))
+        if (((x - 2) > 0) && ((y - 1) > 0))
         {
-            preX=x;
-            preY=y;
             x = x - 2;
             y = y - 1;
         }
         //вправо вниз
-        else if (((x + 1) < size ) && ((y + 2) < size))
+        if (((x + 1) < size ) && ((y + 2) < size))
         {
-            preX=x;
-            preY=y;
-            x = x - 2;
+            x = x + 1;
+            y = y + 2;
+        }
+        //вправо вверх
+        if (((x - 1) <= 0 ) && ((y + 2) < size))
+        {
+            x = x - 1;
+            y = y + 2;
+        }
+        //вниз влево
+        if (((x + 2) < size ) && ((y - 1) >= 0))
+        {
+            x = x + 2;
             y = y - 1;
         }
+        //вниз вправо
+        if (((x + 2) < size ) && ((y + 1) < size ))
+        {
+            x = x + 2;
+            y = y + 1;
+        }
+        //влево вверх
+        if (((x - 1) >= 0 ) && ((y - 2) >= 0))
+        {
+            x = x - 1;
+            y = y - 2;
+        }
+        //влево вниз
+        if (((x + 1) < size ) && ((y - 2) >= 0))
+        {
+            x = x + 1;
+            y = y - 2;
+        }
 
-        return result;
+        return (x, y);
     }
 }
