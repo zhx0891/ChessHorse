7d1591bb7c469bde83a990066e0a1a37b27c9282
zhukovv <northbug@yandex.ru>
2022-09-10 09:02:56 +0500

    horse new version

--- Horse.cs
+++ Horse.cs
@@ -12,10 +12,11 @@ public static class Horse
             }
         }
         System.Console.WriteLine();
-
-
         System.Console.ReadLine();
     }
+
+
+
     public static void meWantToHideIt(int x, int y, int size, int[,] field, int count)
     {
         field[x, y] = 1;
@@ -27,18 +28,62 @@ public static class Horse
             Horse.nextMove(x, y, size, field, count);
         }
     }
-    public static void nextMove(int x, int y, int size, int[,] field, int count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static(int [,] field, int x, int y, bool goodMove,int count) upRight (int x, int y, int[,] field, int count)
     {
         //вверх вправо
-        if ((x - 2) >= 0 & (y + 1) < size)
+        bool goodMove = true;
+        if ((x - 2) >= 0 & (y + 1) < field .GetLength(0)*field.GetLength(1))//проверка существования клетки на поле
         {
-            if (field[x - 2, y + 1] != 1)
+            if (field[x - 2, y + 1] != 1) //проверка клетки на содержимое
             {
                 x = x - 2;
                 y = y + 1;
-                meWantToHideIt(x, y, size, field, count);
+                field[x, y] = 1;
+                count++;
             }
-        }
+
+        } 
+        return (field,x,y,goodMove, count);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void nextMove(int x, int y, int size, int[,] field, int count)
+    {
+        // //вверх вправо
+        // if ((x - 2) >= 0 & (y + 1) < size)
+        // {
+        //     if (field[x - 2, y + 1] != 1)
+        //     {
+        //         x = x - 2;
+        //         y = y + 1;
+        //         meWantToHideIt(x, y, size, field, count);
+        //     }
+        // }
 
         //вверх влево
         if (((x - 2) >= 0) && ((y - 1) >= 0))
@@ -188,7 +233,7 @@ public static class Horse
         //влево вниз
         if (((x + 1) < size) & ((y - 2) >= 0))
         {
-            if (field[x + 2, y + 1] != 1)
+            if (field[x + 1, y - 2] != 1)
             {
                 x = x + 1;
                 y = y - 2;
